db63d6855b62d7f4854626e21f80eb1a1a2d272d
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-02-15 16:37:05 +0300

    Map

--- Ch3/app.js
+++ Ch3/app.js
@@ -1,25 +1,22 @@
 'use strict';
-console.log('hello');
 
-const flights = ['Rus', 'USA', 'UK', 'Japan', 'China', 'UK', 'USA'];
+const weatherMap = new Map();
+weatherMap
+  .set('London', '10')
+  .set('Minsk', '7');
 
-const setFlights = new Set(flights);
-
-console.log(setFlights);
-console.log(setFlights.size);
-console.log(setFlights.has('USA'));
-console.log(setFlights.has('U'));
-setFlights.add('France');
-console.log(setFlights);
-setFlights.delete('Rus');
-console.log(setFlights);
-// setFlights.clear();
-
-// console.log(setFlights);
-
-for(const flight of setFlights){
-  console.log(flight);
-}
-
-console.log([...setFlights]);
+const arr = [1, 2, 3];
+const obj = { a: 1 }
+console.log(weatherMap.has('Minsk'));
+console.log(weatherMap.has('Msc'));
+console.log(weatherMap.get('Minsk'));
+// weatherMap.delete('London')
+// console.log(weatherMap.delete('London'));
+// weatherMap.clear()
+weatherMap.set(arr, 'array')
+weatherMap.set(obj, { b: 2 });
+console.log(weatherMap.get(arr));
+console.log(weatherMap.get(obj));
+console.log(weatherMap);
+console.log(weatherMap.size)
 
