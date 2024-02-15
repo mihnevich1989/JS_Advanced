240a9aabc8b02348d2de95a711f80928e4843706
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-02-15 19:05:19 +0300

    WeakMap

--- Ch3/app.js
+++ Ch3/app.js
@@ -1,11 +1,32 @@
 'use strict';
+let obj = { a: 1 };
+let objB = { b: 2 };
+const wMap = new WeakMap();
 
-const weatherMap = new Map([
-  ['London', 10],
-  ['Minsk', 7],
-  ['Paris', 8]
-]);
-console.log(weatherMap);
-const weatherMap2 = new Map([...weatherMap].map(el => el.reverse()));
-console.log(weatherMap2);
+wMap.set(obj, 'testA');
+wMap.set(objB, 'testB');
 
+console.log(wMap);
+console.log(wMap.get(obj));
+console.log(wMap.has(obj));
+
+wMap.delete(obj);
+
+setTimeout(() => {
+  console.log(wMap);
+}, 1000);
+
+let cache = new WeakMap();
+
+function getValue(obj) {
+  if (!cache.has(obj)) {
+    let res = 1;
+    cache.set(obj, res);
+  }
+  return cache.get(obj);
+}
+
+const res = getValue(objB);
+console.log(res);
+const res2 = getValue(objB);
+console.log(res2);
