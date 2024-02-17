096f3f56281e1cac48a92ba998322742a671c86d
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-02-17 16:51:21 +0300

    22. Упражнение - Функция случайного числа

--- Ch4/app.js
+++ Ch4/app.js
@@ -1,18 +1,6 @@
 'use strict';
 
-console.log(Math.round(1.5));
-console.log(Math.round(1.6));
-console.log(Math.round(1.2));
-console.log(Math.round(1.499999));
-
-console.log(Math.ceil(1.1));
-console.log(Math.ceil(1.2));
-
-console.log(Math.floor(1.8));
-console.log(Math.floor(1.6));
-
-console.log(Math.trunc(1.111));
-console.log(Math.trunc(2.56));
-console.log(Math.trunc(100.77));
-
-console.log(Number((1.4999956).toFixed(1)));
+function btwNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+console.log(btwNumber(2, 3));
