1ddbff7bf565840e44503955f8c3fc482d1211b0
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-02-18 21:28:24 +0300

    25. Работа с BigInt

--- Ch4/app.js
+++ Ch4/app.js
@@ -1,6 +1,20 @@
 'use strict';
 
-const bigNum = 350_500_000;
-const bigNum2 = 350500000;
-console.log(bigNum === bigNum2); // true
+const max = 2 ** 53 - 1;
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(max + 1);
+console.log(max + 2);
+console.log(max + 3);
 
+//bigInt
+console.log(23545123321245612132151231223n);
+console.log(BigInt(23545123321245612132151231223));
+console.log(BigInt('23545123321245612132151231223'));
+
+console.log(10n + 10n);
+//console.log(10n + 10); // error
+//console.log(10.555n); // error
+console.log(BigInt(10) + 10n);
+console.log(23545123321245612132151231223n * 23545123321245612132151231223n);
+console.log(typeof 10n);
