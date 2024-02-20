96ff360f571a6505b890cf5277a513ccfdd5cf4d
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-02-20 12:50:24 +0300

    26. Интернационализация чисел

--- Ch4/app.js
+++ Ch4/app.js
@@ -1,20 +1,30 @@
 'use strict';
 
-const max = 2 ** 53 - 1;
-console.log(Number.MAX_SAFE_INTEGER);
-console.log(Number.MIN_SAFE_INTEGER);
-console.log(max + 1);
-console.log(max + 2);
-console.log(max + 3);
+const options1 = {
+  style: 'currency',
+  currency: 'BYN',
+  useGrouping: false
+};
+const options2 = {
+  style: 'currency',
+  currency: 'USD'
+};
 
-//bigInt
-console.log(23545123321245612132151231223n);
-console.log(BigInt(23545123321245612132151231223));
-console.log(BigInt('23545123321245612132151231223'));
+const options3 = {
+  style: 'decimal'
+};
 
-console.log(10n + 10n);
-//console.log(10n + 10); // error
-//console.log(10.555n); // error
-console.log(BigInt(10) + 10n);
-console.log(23545123321245612132151231223n * 23545123321245612132151231223n);
-console.log(typeof 10n);
+const options4 = {
+  style: 'percent'
+};
+
+const options5 = {
+  style: 'unit',
+  unit: 'celsius' //fahrenheit
+};
+
+console.log(new Intl.NumberFormat('by-BY', options1).format(2300));
+console.log(new Intl.NumberFormat('en-EN', options2).format(2300));
+console.log(new Intl.NumberFormat('ru-RU', options3).format(10000));
+console.log(new Intl.NumberFormat('ru-RU', options4).format(.1));
+console.log(new Intl.NumberFormat('by-BY', options5).format(25));
