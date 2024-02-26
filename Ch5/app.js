63db93aa2802a447462b845988f7f29d4509b714
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-02-26 18:56:59 +0300

    30. Сравнение дат

--- Ch5/app.js
+++ Ch5/app.js
@@ -1,13 +1,9 @@
 'use strict';
 
-const date1 = new Date(2024, 10, 15);
-const date2 = new Date(2024, 11, 15);
-console.log(+date1);
-console.log(Number(date1));
-console.log(date2 - date1);
-
-function getDaysBetweenDates(dateF, dateS) {
-  return (dateS - dateF) / (1000 * 60 * 60 * 24);
-}
-
-console.log(getDaysBetweenDates( date1, date2));
+const first = new Date(2024, 10, 4);
+const second = new Date(2024, 10, 4);
+console.log();
+console.log(first > second);
+console.log(first.getTime() == second.getTime());
+console.log(first.getTime() === second.getTime());
+console.log(Number(first) === Number(second));
