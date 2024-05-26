c913c4977ab10cfbbbb70822d5ff3770f388043e
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-05-26 16:13:44 +0300

    42. Упражнение - Корзина товаров

--- Ch7/app.js
+++ Ch7/app.js
@@ -1,32 +1,4 @@
 'use strict';
-
-/* const Book = function (title, author) {
-  this.title = title;
-  this.author = author;
-  this.isRead = false;
-};
-
-Book.prototype.toggleRead = function () {
-  this.isRead = !this.isRead;
-};
-Book.prototype.cover = "Paper";
-
-const book1 = new Book('The Hobbit', 'J.R.R. Tolkien');
-
-
-console.log(book1);
-book1.toggleRead();
-console.log(book1);
-
-console.log(book1.cover);
-console.log(book1.hasOwnProperty('cover'));
-console.log(book1.hasOwnProperty('author'));
-
-console.log(book1.__proto__ === Book.prototype);
-
-console.log(Book.prototype.isPrototypeOf(book1));
- */
-
 const prod1 = { id: 1, name: 'Bread', count: 1 };
 const prod2 = { id: 3, name: 'Meat', count: 3 };
 
