b0a41e9c10b2c0df18e4055116cbcf6bed06342b
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-05-28 10:38:31 +0300

    46. Static

--- Ch8/app.js
+++ Ch8/app.js
@@ -1,44 +1,20 @@
 'use strict';
 
-const task = {
-  title: 'Task1',
-  dueTo: new Date('2024/01/01'),
-
-  get isOverdue() {
-    return this.dueTo < new Date();
-  },
-
-  set isOverdue(isOverdueTask) {
-    if (!isOverdueTask) {
-      this.dueTo = new Date();
-    }
+class Test {
+  static a = 1;
+  static hello(){
+    console.log('Hello');
   }
+
+  static {
+    let b = 5;
+    this.a += b
+  }
+
 };
 
-console.log(task.isOverdue);
-task.isOverdue = false;
-console.log(task);
+const test = new Test();
 
+Test.hello()
 
-class Task {
-  constructor(title, dueDate) {
-    this.title = title;
-    this.dueDate = dueDate;
-  }
-
-  get isOverdue() {
-    return this.dueDate < new Date();
-  }
-
-  set dueDate(date) {
-    if (date < new Date()) {
-      return;
-    }
-
-    this._dueDate = date;
-  }
-
-}
-const newTask = new Task('TASK 2', new Date());
-console.log(newTask.dueDate = new Date('2023/1/1'));
-console.log(newTask);
+console.log(Test.a);
