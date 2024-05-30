0de8a38f8cd8e61c50d7da7b3d15e8c4d0964ee6
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-05-30 10:49:33 +0300

    51. Наследование

--- Ch9/app.js
+++ Ch9/app.js
@@ -1,40 +1,31 @@
 'use strict';
 
-/* Абстракция VS Инкапсуляция
-  - Название
-  - Режисер
-  - Рейтинг
-  - Продолжительность
-  - Страна производства
-  - Актеры
-  - Трейлер
-  ...
-  
-*/
+const Book = function (title, author) {
+  this.title = title;
+  this.author = author;
+};
 
-class Film {
-  #name;
-  #author;
-  rating;
-  #length;
+Book.prototype.getDescription = function () {
+  console.log(`${this.title} was written by ${this.author}.`);;
+};
 
-  constructor(name, author, length) {
-    this.#name = name;
-    this.#author = author;
-    this.#length = length;
-  }
+const AudioBook = function (title, author, runtime) {
+  Book.call(this, title, author);
+  this.runtime = runtime;
+};
 
-  get name() {
-    return this.#name;
-  }
-  get author() {
-    return this.#author;
-  }
-  get length() {
-    return this.#length;
-  }
+AudioBook.prototype = Object.create(Book.prototype);
+AudioBook.prototype.constructor = AudioBook;
+AudioBook.prototype.log = function () {
+  console.log(`runtime of ${this.title} book is ${this.runtime}`);
+};
 
-}
+const book = new AudioBook('LOTR', 'Tolkien', 20 * 60);
 
-const film = new Film('Avatar', 'Cameron', 240);
-console.log(film);
+book.log();
+book.getDescription();
+
+console.log(book);
+
+console.log(book instanceof AudioBook);
+console.log(book instanceof Book);
