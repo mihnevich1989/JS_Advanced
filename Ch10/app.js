3bab15a73984b0f499d4d062a843b6a74cacdb8d
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-06-03 10:38:28 +0300

    58. Принцип единой ответственности

---
+++ Ch10/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+class Character {
+  #inventory = [];
+  #health = 10;
+
+  pickItem(item) {
+    this.#inventory.push(item);
+  }
+
+  recieveDamage(damage) {
+    this.#health -= damage;
+  }
+
+}
+
+class DB {
+  save(item) {
+    localStorage.setItem('char', item);
+  }
+
+  load() {
+    //....
+  }
+}
