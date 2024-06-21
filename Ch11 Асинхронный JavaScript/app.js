e8ce9cbaada3866b17906f8f0955bcc2270295f3
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-06-21 21:53:01 +0300

    71. Обработка promises

--- Ch11 Асинхронный JavaScript/app.js
+++ Ch11 Асинхронный JavaScript/app.js
@@ -1,16 +1,13 @@
 'use strict';
 
-function req() {
-  const request = new XMLHttpRequest();
+const request = new XMLHttpRequest();
 
-  request.open('GET', 'https://dummyjson.com/products/');
-  request.send();
+request.open('GET', 'https://dummyjson.com/products/');
+request.send();
 
-  request.addEventListener('load', function () {
-    const { products } = JSON.parse(this.responseText);
-    const sum = products.reduce((acc, product) => acc += product.price, 0);
-    console.log(sum / products.length);
-  });
-}
+request.addEventListener('load', function () {
+  const { products } = JSON.parse(this.responseText);
+  const sum = products.reduce((acc, product) => acc += product.price, 0);
+  console.log(sum / products.length);
+});
 
-req();
--- Ch12 Promises/app.js
+++ Ch12 Promises/app.js
@@ -1,1 +1,7 @@
 'use strict';
+
+fetch('https://dummyjson.com/products/1')
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  });
