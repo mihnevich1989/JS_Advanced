0886d74f633bd34340ab90379eac837a2b723cd8
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-06-24 09:12:46 +0300

    73. Обработка reject

--- Ch12 Promises/app.js
+++ Ch12 Promises/app.js
@@ -1,9 +1,10 @@
 'use strict';
 
-fetch('https://dummyjson.com/products')
+fetch('https://dummyjson.com/productss')
   .then(res => res.json())
   .then(({ products }) => {
     return fetch('https://dummyjson.com/products/' + products[0].id);
   })
   .then(res => res.json())
-  .then(data => console.log(data));
+  .then(data => console.log(data))
+  .catch(err => console.log(err))
