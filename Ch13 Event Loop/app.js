ccf4dd8725a6bdfe2c866cd0a0e533af4cdc3c2e
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-06-25 10:55:29 +0300

    80. Создание простого Promise

--- Ch13 Event Loop/app.js
+++ Ch13 Event Loop/app.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const prom = new Promise((resolve, reject) => {
+  if (new Date() < new Date('01/01/2025')) {
+    reject(new Error('Error'));
+  }
+  resolve('Success');
+});
+
+prom
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+
+function timeout(sec) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, sec * 1000);
+  });
+}
+
+timeout(1)
+  .then(() => {
+    console.log(1);
+    return timeout(2);
+  }).then(() => {
+    console.log(2);
+  });
