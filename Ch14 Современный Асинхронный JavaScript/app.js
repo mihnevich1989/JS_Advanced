5982d9c818f9c095ae80baab128729c0596eb39d
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-07-09 09:48:51 +0300

    83. Async await

--- Ch14 Современный Асинхронный JavaScript/app.js
+++ Ch14 Современный Асинхронный JavaScript/app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+async function getProducts() {
+  const resProducts = await fetch('https://dummyjson.com/products');
+  const { products } = await resProducts.json();
+  console.log(products);
+
+  const resProduct = await fetch('https://dummyjson.com/product/' + products[0].id);
+  const product = await resProduct.json();
+  console.log(product);
+}
+
+getProducts()
