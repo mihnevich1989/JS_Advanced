94718e9dad517e3ce2f20ff1e233c388f0161715
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2024-07-09 10:05:16 +0300

    84. Try catch

--- Ch14 Современный Асинхронный JavaScript/app.js
+++ Ch14 Современный Асинхронный JavaScript/app.js
@@ -1,13 +1,21 @@
 'use strict';
 
 async function getProducts() {
-  const resProducts = await fetch('https://dummyjson.com/products');
-  const { products } = await resProducts.json();
-  console.log(products);
-
-  const resProduct = await fetch('https://dummyjson.com/product/' + products[0].id);
-  const product = await resProduct.json();
-  console.log(product);
+  try {
+    const resProducts = await fetch('https://dummyjson.com/products');
+    if (!resProducts.ok) {
+      throw new Error(resProducts.status);
+    }
+    const { products } = await resProducts.json();
+    console.log(products);
+    const resProduct = await fetch('https://dummyjson.com/product/' + products[0].id);
+    const product = await resProduct.json();
+    console.log(product);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log('Finally');
+  }
 }
 
-getProducts()
+getProducts();
