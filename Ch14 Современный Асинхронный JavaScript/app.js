18e510c5c44601a625756ee652f0f1a547e5e43d
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2025-09-08 11:12:53 +0300

    89. Другие комбинации Promise

--- Ch14 Современный Асинхронный JavaScript/app.js
+++ Ch14 Современный Асинхронный JavaScript/app.js
@@ -10,11 +10,28 @@ async function getProductsById(id) {
   return response.json();
 }
 
+async function getProductsByIdWithError(id) {
+  const response = await fetch('https://dummyjson.coms/products/' + id);
+  return response.json();
+}
+
 async function main() {
   const { products } = await getAllProducts();
-  const res = await Promise.all(products.map(product => getProductsById(product.id)));
+  // const res = await Promise.all(products.map(product => getProductsById(product.id)));
 
-  console.log(res);
+  const resWithoutError = await Promise.allSettled([
+    getProductsById(1),
+    getProductsById(2)
+  ]);
+
+  const resWithError = await Promise.allSettled([
+    getProductsById(1),
+    getProductsByIdWithError(2)
+  ]);
+
+  console.log(resWithoutError);
+  console.log(resWithError);
+
 
   /* for (const product of products) {
     const resp = await getProductsById(product.id);
