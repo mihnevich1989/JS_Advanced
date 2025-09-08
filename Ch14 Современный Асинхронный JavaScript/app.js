8f752a7ecd3394dba9e8f9442bebca93c9166528
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2025-09-08 11:19:36 +0300

    90. Все возможности fetch

--- Ch14 Современный Асинхронный JavaScript/app.js
+++ Ch14 Современный Асинхронный JavaScript/app.js
@@ -1,43 +1,20 @@
 'use strict';
 
-async function getAllProducts() {
-  const response = await fetch('https://dummyjson.com/products');
-  return response.json();
-}
-
-async function getProductsById(id) {
-  const response = await fetch('https://dummyjson.com/products/' + id);
-  return response.json();
-}
-
-async function getProductsByIdWithError(id) {
-  const response = await fetch('https://dummyjson.coms/products/' + id);
-  return response.json();
-}
-
 async function main() {
-  const { products } = await getAllProducts();
-  // const res = await Promise.all(products.map(product => getProductsById(product.id)));
+  const response = await fetch('https://dummyjson.com/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: 'emilys',
+      password: 'emilyspass'
+    })
+  });
 
-  const resWithoutError = await Promise.allSettled([
-    getProductsById(1),
-    getProductsById(2)
-  ]);
-
-  const resWithError = await Promise.allSettled([
-    getProductsById(1),
-    getProductsByIdWithError(2)
-  ]);
-
-  console.log(resWithoutError);
-  console.log(resWithError);
-
-
-  /* for (const product of products) {
-    const resp = await getProductsById(product.id);
-    console.log(resp);
-  } */
-
+  const data = await response.json();
+  console.log(data);
+  
 }
 
 main();
